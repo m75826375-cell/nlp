d26b7e6efeca246c544a9e90d7e6bb7e14e56996
m75826375-cell <m75826375@gmail.com>
2026-03-29 15:09:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
-model_path = r"https://drive.google.com/drive/folders/1AAu4rU5aYSzpnnT7wQ1iHRYdPefkpPaA?usp=drive_link" # Update with your NER model folder path
-C_MODEL_PATH = r"https://drive.google.com/drive/folders/1eRUtxtS_hNj6VVewr9AFilEE_heVyKAl?usp=drive_link" # Update with your Severity classifier model folder path
+model_path = r"trijesh/ade-ner-model" # Update with your NER model folder path
+C_MODEL_PATH = r"trijesh/ade-severity-model" # Update with your Severity classifier model folder path
 
